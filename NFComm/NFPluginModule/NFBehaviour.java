3d88a0ab9733d146094ed676c96647f61f1c15bf
kytooooo <342006@qq.com>
2015-10-30 18:04:56 +0800

    add behaviour

---
+++ NFComm/NFPluginModule/NFBehaviour.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package nframe;
+
+/**
+ * @lvsheng.huang
+ */
+public class NFBehaviour {
+
+	public abstract void init();
+
+    public abstract void afterInit();
+
+    public abstract void beforeShut();
+
+    public abstract void shut();
+
+    public abstract void execute();
+}
