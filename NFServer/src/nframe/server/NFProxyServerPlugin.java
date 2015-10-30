59589b88ee0cd3ceb2c6aca583a8a41b0698a94a
xiongxiaolei <34894419@qq.com>
2015-10-30 21:16:43 +0800

    init commit

---
+++ NFServer/src/nframe/server/NFProxyServerPlugin.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package nframe.server;
+
+/**
+ * @author Xiong
+ * 代理服务器插件
+ */
+public class NFProxyServerPlugin {
+
+}
---
+++ NFServer/test/nframe/server/test/ProxyServer.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package nframe.server.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Xiong
+ * 代理服务器测试
+ */
+public class ProxyServer {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
