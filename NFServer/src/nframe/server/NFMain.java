39b4db3fa9a4a85013849971d1777f3a6ddf4a45
xiongxiaolei <34894419@qq.com>
2015-10-31 18:09:39 +0800

    add NFMain

---
+++ NFServer/src/nframe/server/NFMain.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package nframe.server;
+
+/**
+ * @author Xiong
+ * NFServer的入口
+ */
+public class NFMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
