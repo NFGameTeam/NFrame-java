8af52be6bb4d76d6b09864196e7d5168b28ee0c8
kytooooo <342006@qq.com>
2015-11-18 23:38:53 +0800

    add element info mng

--- NFCore/src/nframe/NFElementInfoModule.java
+++ NFCore/src/nframe/NFElementInfoModule.java
@@ -10,6 +10,90 @@ import java.util.List;
  * 框架核心,数据类
  */
 
-public abstract class NFElementInfoModule extends NFIElementInfoModule{
+public class NFElementInfoModule extends NFIElementInfoModule{
+	
+	@Override
+	public void init()
+	{
+		   //m_pLogicClassModule = dynamic_cast<NFCLogicClassModule*>(pPluginManager->FindModule("NFCLogicClassModule"));
 
+		   //assert(NULL != m_pLogicClassModule);
+	}
+	@Override
+	public void afterInit()
+	{
+		
+	}
+	@Override
+	public void beforeShut()
+	{
+		Clear();
+	}
+	@Override
+	public void shut()
+	{
+		
+	}
+	@Override
+	public void execute()
+	{
+		
+	}
+	@Override
+	public boolean Load()
+	{
+/*		NF_SHARED_PTR<NFILogicClass> pLogicClass = m_pLogicClassModule->First();
+	    while (pLogicClass.get())
+	    {
+			Load(pLogicClass);
+
+	        mbLoaded = true;
+
+	        pLogicClass = m_pLogicClassModule->Next();
+	    }*/
+	    
+		return true;
+	}
+	@Override
+	public boolean Clear()
+	{
+		return false;
+	}
+
+	@Override
+	public boolean LoadSceneInfo(String strFileName, String strClassName)
+	{
+		return false;
+	}
+	@Override
+	public boolean Load(String strClassName)
+	{
+		return false;
+	}
+	@Override
+	public boolean ExistElement(String strConfigName)
+	{
+		return false;
+	}
+	@Override
+	public NFIPropertyManager GetPropertyManager(String strConfigName)
+	{
+		return null;
+	}
+	// public abstract NFIRecordManager GetRecordManager(String strConfigName);
+	@Override
+	public long GetPropertyInt(String strConfigName, String strPropertyName)
+	{
+		return NFIData.INT_NIL;
+	}
+	@Override
+	public double GetPropertyFloat(String strConfigName, String strPropertyName)
+	{
+		return NFIData.FLOAT_NIL;
+	}
+	@Override
+	public String GetPropertyString(String strConfigName, String strPropertyName)
+	{
+		return NFIData.STRING_NIL;
+	}
 }
--- NFCore/src/nframe/NFIElementInfoModule.java
+++ NFCore/src/nframe/NFIElementInfoModule.java
@@ -6,10 +6,27 @@ package nframe;
 import java.util.List;
 
 /**
- * @author lvsheng.huang
- * 框架核心,逻辑类接口
+ * @author lvsheng.huang 框架核心,逻辑类接口
  */
 
-public abstract class NFIElementInfoModule  extends NFBehaviour {
+public abstract class NFIElementInfoModule extends NFBehaviour {
+	public abstract boolean Load();
 
+	public abstract boolean Clear();
+
+	// special
+	public abstract boolean LoadSceneInfo(String strFileName, String strClassName);
+
+	public abstract boolean Load(String strClassName);
+
+	public abstract boolean ExistElement(String strConfigName);
+
+	public abstract NFIPropertyManager GetPropertyManager(String strConfigName);
+	// public abstract NFIRecordManager GetRecordManager(String strConfigName);
+
+	public abstract long GetPropertyInt(String strConfigName, String strPropertyName);
+
+	public abstract double GetPropertyFloat(String strConfigName, String strPropertyName);
+
+	public abstract String GetPropertyString(String strConfigName, String strPropertyName);
 }
