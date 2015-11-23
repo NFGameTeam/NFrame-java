702b147d8eb1862a8f5220bf310fcca877bf9a37
kytooooo <342006@qq.com>
2015-11-23 20:30:51 +0800

    add element

---
+++ NFCore/src/nframe/NFElement.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package nframe;
+
+/**
+ * @author lvsheng.huang 框架核心,基础元素类
+ */
+
+public class NFElement extends NFIElement {
+	
+	NFIPropertyManager propertyManager;
+	//NFIRecordManager recordManager;
+	
+	
+	@Override
+	public NFIPropertyManager getPropertyManager()
+	{
+		return this.propertyManager;
+	}
+	
+/*	@Override
+	public NFIRecordManager getRecordManager()
+	{
+		return this.recordManager;
+	}*/
+	
+}
