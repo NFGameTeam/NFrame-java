7586f2957a3f279840d001ab60ce9fbc96b3c40b
kytooooo <342006@qq.com>
2015-12-05 16:43:16 +0800

    adjust folder for plugin

---
+++ NFConfigPlugin/src/nframe/NFConfigPlugin.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package nframe;
+
+/**
+ * @author lvsheng.huang 配置插件
+ */
+
+public class NFConfigPlugin extends NFIPlugin{
+	
+	@Override
+	public void install()
+	{
+		
+	}
+	
+	/**
+	 * 反安装自身
+	 */
+	@Override
+	public void uninstall()
+	{
+		
+	}
+	@Override
+	public void init()
+	{
+		
+	}
+	@Override
+	public void afterInit()
+	{
+		
+	}
+	@Override
+	public void beforeShut()
+	{
+		
+	}
+	@Override
+	public void shut()
+	{
+		
+	}
+	@Override
+	public void execute()
+	{
+		
+	}
+}
