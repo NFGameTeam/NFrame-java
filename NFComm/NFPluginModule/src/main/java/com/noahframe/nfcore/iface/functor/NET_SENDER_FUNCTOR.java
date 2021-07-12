172eb93a39fe748599ab559af1693ee4213719d2
zoocee <yideal_formula@126.com>
2021-07-12 17:37:25 +0800

    重构之前清除代码

--- NFComm/NFPluginModule/src/main/java/com/noahframe/nfcore/iface/functor/NET_SENDER_FUNCTOR.java
+++
@@ -1,6 +0,0 @@
-package com.noahframe.nfcore.iface.functor;
-
-public interface NET_SENDER_FUNCTOR {
-
-	<T> void operator(int nSockIndex, T msg);
-}
--- NFComm/NFPluginModule/src/main/java/com/noahframe/nfcore/iface/module/NFISocketModule.java
+++
@@ -1,8 +0,0 @@
-package com.noahframe.nfcore.iface.module;
-
-import java.util.Map;
-
-
-public abstract class NFISocketModule extends NFINetModule{
-
-}
